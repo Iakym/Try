fb9992875afdd942281eb1a7962b63b9b621e6e0
yakym <iakymos@gmail.com>
2022-10-06 12:15:25 +0300

    malo
    add malo

--- python.py
+++ python.py
@@ -4,9 +4,11 @@ def loopp(lisst):
     for i in lisst:
         if type(i) is int:
             print(f'Цифра {i}')
+            if i<=0:
+                print(f'{i} мало')
         else:
             print(f'Буква {i}')
 
-loopp([1,2,'f',6])
+loopp([1,2,'f',6,-5])
 
 
