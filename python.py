9b31ad2f72a1f208f5d483f3307520f2eae28d8a
yakym <iakymos@gmail.com>
2022-10-06 12:06:07 +0300

    func
    make a funktion

--- python.py
+++ python.py
@@ -1,10 +1,12 @@
-a = [1,'e','3',4]
 
-for i in a:
-    if type(i) is int:
-        print(f'Цифра {i}')
-    else:
-        print(f'Буква {i}')
 
+def loopp(lisst):
+    for i in lisst:
+        if type(i) is int:
+            print(f'Цифра {i}')
+        else:
+            print(f'Буква {i}')
+
+loopp([1,2,'f',6])
 
 
